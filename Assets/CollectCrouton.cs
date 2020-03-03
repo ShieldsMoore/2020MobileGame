6981fb8840160cf2e82cd227500ae4afa60fd223
ShieldsMoore <49412632+ShieldsMoore@users.noreply.github.com>
2020-03-02 21:11:41 -0500

    More Edits

---
+++ Assets/CollectCrouton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCrouton : MonoBehaviour
+{
+    public bool ownFake;
+    public int fake;
+    public Rigidbody2D cRB;
+    public SpriteRenderer sr;
+    public SpriteRenderer sr2;
+    public GameObject securitycam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("Fake", 1);  //only do this when you buy the fake crouton
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fake = PlayerPrefs.GetInt("Fake");
+        if(fake > 0)
+        {
+            ownFake = true;
+        }
+        else
+        {
+            ownFake = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(ownFake == true)
+            {
+                sr.enabled = false;
+                sr2.enabled = true;
+                PlayerPrefs.SetInt("Fake", 0);
+                //also the player gets a big crouton bonus
+            }
+
+            else if (ownFake == false)
+            {
+                sr.enabled = false;
+                cRB.mass = 1;
+                //the player still gets a big crouton bonus
+                //but the alarm is triggered because we didn't replace it
+            }
+        }
+    }
+
+    public void DisableSecurity()
+    {
+        securitycam.SetActive(false);
+    }
+}
---
+++ Assets/WeightedCruoton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightedCruoton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if( collision.gameObject.tag == "Crouton")
+        {
+            if(collision.gameObject.GetComponent<Rigidbody2D>().mass > 2)
+            {
+                Debug.Log("we are safe");
+            }
+            else if (collision.gameObject.GetComponent<Rigidbody2D>().mass <= 2)
+            {
+                Debug.Log("we are in trouble");
+            }
+        }
+    }
+}
